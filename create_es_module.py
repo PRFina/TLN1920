ae22eb432390b8453e1fa95a4bd4ce70863c4744
PRFina <pioraffaelefina@outlook.it>
2021-04-02 12:27:46 +0200

    Add quick script to create directory structure for new exercises

---
+++ create_es_module.py
@@ -0,0 +1,27 @@
+"""QUick script to use when start a new exercise module. Just call script with
+python create_es_module <numero esercitazione>
+"""
+
+from pathlib import Path
+import sys
+MODULE_NAME =  'esercitazione_' + sys.argv[1]
+DATA_NAME = 'data'
+OUTPUT_NAME = 'output'
+CODE_NAME = 'src'
+
+top_level = [Path(DATA_NAME), 
+            Path(OUTPUT_NAME),
+            Path(CODE_NAME)]
+
+module_path = Path(MODULE_NAME)
+module_path.mkdir()
+
+for dir in top_level:
+    (module_path / dir).mkdir()
+
+code_path = top_level[2]
+(module_path / code_path / '__init__.py').open(mode='w') # init file for src package
+
+(module_path / MODULE_NAME).with_suffix('.ipynb').open(mode='w') # init file for src package
+
+
