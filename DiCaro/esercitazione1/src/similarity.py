4b3702fd1336cc57282f23834938c6bc1079d926
Pio Raffaele Fina <pio.fina@edu.unito.it>
2021-05-10 22:09:31 +0000

    add esercitazione 1 Dicaro

---
+++ DiCaro/esercitazione1/src/similarity.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import nltk as nltk
+from nltk.corpus import stopwords
+import numpy as np
+from pathlib import Path
+import itertools as it
+
+
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+
