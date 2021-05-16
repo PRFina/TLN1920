6a8f43831093222ab46b88a5d8e6f51e8c1519c4
PRFina <pioraffaelefina@outlook.it>
2021-05-17 01:41:25 +0200

    Add markdown comments and batch processing in esercitazione4

--- DiCaro/esercitazione4/src/data_manager.py
+++ DiCaro/esercitazione4/src/data_manager.py
@@ -2,20 +2,36 @@
 from pathlib import Path
 import numpy as np
 
+from nltk.tokenize import RegexpTokenizer
+import re
+
 class Document:
-    BLOCK_SEPARATOR = "$$^^$$"
+    BLOCK_SEPARATOR = "========"
+    #WIKI_727K_SEPARATOR_REGX = "========,*[0-9]*,*[\w+\s*]*\.+"
+    # match anything in between 8 consecutives '=' and a newline 
+    WIKI_727K_SEPARATOR_REGX = "========.\n*" 
 
-    def __init__(self, doc_file, tokenizer):
-        self.tokenizer = tokenizer
+    def __init__(self, doc_file, tokenizer, search_title=False):
+        self._chunk_tokenizer = tokenizer
+        self._section_tokenizer = RegexpTokenizer(Document.WIKI_727K_SEPARATOR_REGX, gaps=True,
+                                                  flags=re.UNICODE | re.MULTILINE)
 
+        
         with doc_file.open() as file:
-            paragraphs = file.read().split('\n\n') # we implicitely assume that double paragraph is the "golden label" for topic separation
-            self.title = paragraphs[0]
-            self.body_paragraphs = paragraphs[1:]
+            self._sections = self._section_tokenizer.tokenize(file.read())
+            sections = [section.strip() for section in self._sections] # remove newlines
+            
+            if search_title:
+                self.title = sections[0]
+                self.body_sections = sections[1:]
+            else:
+                self.title = "unknow document title"
+                self.body_sections = sections
+            
             self._chunks = []
 
-            for para in self.body_paragraphs:
-                self._chunks.extend(self.tokenizer(para))
+            for section in self.body_sections:
+                self._chunks.extend(self._chunk_tokenizer(section))
                 self._chunks.append(Document.BLOCK_SEPARATOR)
             self._chunks = self._chunks[0:-1] # remove last separator
     
